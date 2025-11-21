4d2c716b26e2e651483ed134563eaf84c3c7a5f5
sandarusadishan <sandarusadishan0404@gmail.com>
2025-11-21 19:00:30 +0530

    check now

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base : process.env.VITE_BASE_URL || "/softvision",
+  base: "/", // <-- FIXED
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

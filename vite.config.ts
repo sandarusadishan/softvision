3584fa5e0ae75d1fe8301a10d345ca9cd5a87d30
sandarusadishan <sandarusadishan0404@gmail.com>
2025-11-21 18:54:06 +0530

    CHANGE VITE FILE 👍

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base : process.env.VITE_BASE_URL || "/",
+  base : process.env.VITE_BASE_URL || "/softvision",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

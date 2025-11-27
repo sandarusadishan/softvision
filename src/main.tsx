e5c3e2147b1e232782a47c06a317d29cb05c5b13
sandarusadishan <sandarusadishan0404@gmail.com>
2025-11-27 16:39:33 +0530

    create smoothly loging type

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import React from "react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Smoothly remove the pre-loader
+const loader = document.getElementById("loader");
+if (loader) {
+  loader.classList.add("fade-out");
+  loader.addEventListener("transitionend", () => loader.remove());
+}
